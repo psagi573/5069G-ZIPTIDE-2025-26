d0b542550f9a34408cb76fc3f399f2be071b37f3
Prajwal Sagi <prajwalsagi11@gmail.com>
2025-10-26 09:27:37 -0500

    Removed all non escential files

--- include/Auton.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "vex.h"
-
-void autonomous_safe();
-void autonomous_skills();
-void autonomous_left_awp();
-void autonomous_right_awp();
-void autonomous_left_elim();
-void autonomous_right_elim();
